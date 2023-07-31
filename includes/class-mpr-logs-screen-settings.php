e1b503116ee24c1f5559a8dd26d84ed022c3c979
levchenko_d <levchenkodarja@gmail.com>
2023-07-31 15:29:35 +0200

    fixed saving of rows per page preference on logs page

--- includes/class-mpr-logs-screen-settings.php
+++ includes/class-mpr-logs-screen-settings.php
@@ -6,7 +6,7 @@
         {
             add_filter( "manage_toplevel_page_mpr-plugin-page_columns", [MPR_Logs_Table::class, 'get_columns_list'] );
             add_action("load-toplevel_page_mpr-plugin-page", [self::class, 'add_table_options'] );
-            add_filter( 'set-screen-option', [self::class, 'set_option'], 11, 3 );
+            add_filter( "set-screen-option", [self::class, 'set_option'], 11, 3 );
         }
 
         public static function add_table_options()
@@ -20,8 +20,13 @@
 
         public static function set_option( $status, $option, $value )
         {
-            if ( isset( $_POST['wp_screen_options_nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wp_screen_options_nonce'] ) ), 'wp_screen_options_nonce' ) ) {
-
+            if ( ! empty( $_POST['screenoptionnonce'] ) ) {
+                $nonce_value = sanitize_text_field( wp_unslash( $_POST['screenoptionnonce'] ) );
+                if ( wp_verify_nonce( $nonce_value, 'screen-options-nonce' ) ) {
+                    if ('mpr_rows_per_page' == $option) {
+                        return (int)$value;
+                    }
+                }
             }
 
             return $value;
